65b0c9f5ae5b4caff7b2ee6cb46b9597a3e712f1
domus71 <domus71@gmail.com>
2023-06-29 17:18:55 +0300

    TAS Courier update

--- index.js
+++ index.js
@@ -37,6 +37,30 @@ const acsRequest = async (order) => {
   }
 };
 
+const tasRequest = async (order) => {
+  const url = process.env.TAS_URL;
+
+  try {
+    const response = await axios({
+      url: url + "/hermes_api/pegapi00/hello",
+      method: "get",
+    });
+    const sid = response.data.data.sid;
+    const tracking = await axios({
+      url:
+        url +
+        "/hermes_api/courier/voucher_tracking?sid=" +
+        sid +
+        "&vouchercode[]=" +
+        order.or_postID,
+      method: "get",
+    });
+    return tracking.data.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 const acsCODRequest = async (date) => {
   try {
     var dateString = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
@@ -91,9 +115,35 @@ const checkAndupdate = async (acsResponse, paymentMethod) => {
   }
 };
 
+const checkAndupdateTAS = async (tasResponse, paymentMethod) => {
+  let pay_status = 0;
+  let delivery_date = null;
+  let delivery_consignee = null;
+  try {
+    const obj = tasResponse[0];
+    if (obj.r18p014 === 'ΠΡΔ') {
+      pay_status =
+        parseInt(paymentMethod) === 2 || parseInt(paymentMethod) === 6 ? 1 : 3;
+      delivery_date = obj.r18p117 + ' '+ obj.r18p118;
+      delivery_consignee = obj.r18p119;
+    } else if (obj.r18p014 === 'AKR') {
+      pay_status = 2;
+    }
+    if (pay_status != 0) {
+      await knex("app_orders").where("or_postID", obj.r18p01).update({
+        or_pay_status: pay_status,
+        or_delivery_date: delivery_date,
+        or_delivery_consignee: delivery_consignee,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 (async () => {
-  // Check COD Payments
-  const results = await acsCODRequest(new Date("2022-12-7"));
+  // Check ACS COD Payments
+  const results = await acsCODRequest(new Date());
 
   for (const result of results) {
     await knex("app_orders").where({ or_postID: result.POD }).update({
@@ -101,7 +151,7 @@ const checkAndupdate = async (acsResponse, paymentMethod) => {
     });
   }
 
-  // Check Delivery Status
+  // Check ACS Delivery Status
   const selectedRows = await knex
     .select("or_id", "or_postID", "or_paymethod")
     .from("app_orders")
@@ -110,9 +160,21 @@ const checkAndupdate = async (acsResponse, paymentMethod) => {
     .andWhere("or_pay_status", 0);
   for (const row of selectedRows) {
     let response = await acsRequest(row);
-    //console.log(response);
     if (Array.isArray(response))
       await checkAndupdate(response, row.or_paymethod);
+  }
+  // Check TAS Delivery Status
+  const selectedTASRows = await knex
+    .select("or_id", "or_postID", "or_paymethod")
+    .from("app_orders")
+    .where("or_status", 4)
+    .andWhere("or_deliverymethod", 5)
+    .andWhere("or_pay_status", 0);
+  for (const row of selectedTASRows) {
+    let responseTas = await tasRequest(row);
+    // console.log(responseTas);
+    if (Array.isArray(responseTas))
+      await checkAndupdateTAS(responseTas, row.or_paymethod);
   }
   await knex.destroy();
 })();
